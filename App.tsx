a3655cb111c02d44ee0f5bb689de0229f978ceb3
Maicolls <maickgutierrez13@gmail.com>
2024-07-24 13:04:05 -0500

    aparece un error de <Text>

--- App.tsx
+++ App.tsx
@@ -1,14 +1,50 @@
+// Nuevo componente contenedor con ScrollView
 import React from 'react';
-import Card from './src/Card';
+import {View, ScrollView} from 'react-native';
+import Card from './src/Card'; 
 
-const App = () => {
+const CardsContainer: React.FC = () => {
   return (
-    <>
-      <Card />
-      <Card />
-      <Card />
-    </>
+    <ScrollView>
+      <View>
+        {/* Renderiza aquí múltiples componentes Card con diferentes props */}
+        <Card
+          imageSource={require('./src/Images/linea106.png')}
+          cardTitle="Línea de Atención 106"
+          cardText="Atención, ayuda, intervención psicosocial y/o soporte en situaciones de crisis.
+          Disponible las 24 horas del día, los 7 días de la semana."
+          phoneNumber="106"
+        />
+        <Card
+          imageSource={require('./src/Images/lineacalma.png')}
+          cardTitle="Línea Calma"
+          cardText="Espacio de escucha y asesoría para manejar situaciones emocionales como ansiedad, depresión,
+          entre otros. Lunes a viernes de 7:00 a.m. a 10:30 p.m.,
+          sábados 8:00 a.m. a 10:30 p.m. y domingos 2:00 p.m. a 10:30 p.m.."
+          phoneNumber="01 8000 423 614"
+        />
+        <Card
+          imageSource={require('./src/Images/lineapurpura.png')}
+          cardTitle="Línea Púrpura"
+          cardText="Atención psicológica gratuita las 24 horas del día, para mujeres víctimas de violencia."
+          phoneNumber=" 01 8000 112 137"
+        />
+        <Card
+          imageSource={require('./src/Images/linea192.png')}
+          cardTitle="Salud Mental - Opción 4"
+          cardText="Línea 192. Ofrece primeros auxilios psicológicos y orientación en salud mental. Lunes a viernes de 7:00 a.m. a 7:00 p.m."
+          phoneNumber="192"
+        />
+        <Card
+          imageSource={require('./src/Images/Lineapsico.png')}
+          cardTitle="Línea Psicoactiva"
+          cardText="Aborda el consumo de sustancias psicoactivas. Horario de atención: 8:00 a.m. a 12:00 p.m. y 2:00 p.m. a 5:30 p.m.."
+          phoneNumber="01 8000 112 439"
+        />
+        {/* Añade más cartas según sea necesario */}
+      </View>
+    </ScrollView>
   );
 };
 
-export default App;
+export default CardsContainer;
--- src/Card.tsx
+++ src/Card.tsx
@@ -1,49 +1,73 @@
 import React from 'react';
-import { View, Text, Image, StyleSheet } from 'react-native';
+import {
+  ScrollView,
+  Text,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+  Linking,
+} from 'react-native';
 
-const Card: React.FC = () => {
+//El llamado que se va arealizar en las tarjetas para que sean despues llamados y modificados en el partado de abajo
+interface CardProps {
+  imageSource: any; //Aca se indica que es donde se van a llamar las imagenes que se van a atulizar 
+  cardTitle: string; //Aca se indica que es ele apratado para el titulo de las  cards
+  cardText: string; //Aca se indica que es ele apratado para la informacion  de las lineas de atencion que contiene las  cards
+  phoneNumber: string; // Nueva propiedad para el número de teléfono
+}
+
+const Card: React.FC<CardProps> = ({
+  imageSource, //Aca se llama el interface props para adicionar despues las imagenes
+  cardTitle, //Aca se llama el interface props para adicionar despues el titulo
+  cardText, // Aca se llama el interface props para adicionar despues el texto
+  phoneNumber, //Aca se llama el interface props para adicionar despues la imagenes
+}) => {
+  // Función para abrir el marcador con el número de teléfono
+  const handlePress = () => { 
+    Linking.openURL(`tel:${phoneNumber}`); //Aca es donde se define la funcion de LinKing que es una liberia de React-Native para poder interactuar con enlaces de aplicaciones entrantes y salientes 
+  };
   return (
-    <View style={styles.cardInfo}>
-      <Image style={styles.cardImage} source={require('./Images/linea106_0.jpg')} />
-      <Text style={styles.cardTitle}>¿Necesitas ayuda profesional?</Text>
-      <Text style={styles.cardText}>
-        Aca encontraras la linea de atecion 106 comunicate para que recibas la
-        ayuda que necesitas.
-      </Text>
-    </View>
+    <TouchableOpacity style={styles.cardInfo} onPress={handlePress}>
+      {' '}
+      {/* Aca se llama al evento TouchableOpacity como padre para que este sea el padre en todas las tarjetas que se estan implementando */}
+      <ScrollView style={styles.cardInfo}>
+        {' '}
+        {/*En este evento se debe dejar al igual que el padre debido a que es el que nos permite porder dar el eventro scroll para deslizar las tarjetas */}
+        <Image style={styles.cardImage} source={imageSource} />{' '}
+        {/*esto como es evidente es para las imagenes de las tarjetas */}
+        <Text style={styles.cardTitle}>{cardTitle}</Text>{' '}
+        {/*de igual manera que el apartado de las imagenes tanto esta liena 34 como la 35 son quienes contienen el titulo de las tarjetas y el texto mismo */}
+        <Text style={styles.cardText}>{cardText}</Text>
+      </ScrollView>
+    </TouchableOpacity>
   );
 };
 
+//este son los estilos quqe se le dan a las distintas tarjetas 
 const styles = StyleSheet.create({
-    cardInfo: {
-      borderWidth: 1,
-      borderColor: 'hsl(0, 0%, 80%)',
-      borderRadius: 10,
-      shadowColor: '#000',
-      shadowOffset: { width: 5, height: 5 },
-      shadowOpacity: 0.1,
-      shadowRadius: 5,
-      padding: 20,
-      alignItems: 'center',
-      maxWidth: 250,
-      margin: 10,
-    },
-    cardImage: {
-      borderRadius: 100, // To make it circular
-      marginBottom: 10,
-      width: '60%',
-      height: undefined,
-      aspectRatio: 1, // Keep the aspect ratio of the image
-    },
-    cardTitle: {
-      fontFamily: 'Arial',
-      margin: 0,
-      color: 'hsl(0, 0%, 20%)',
-    },
-    cardText: {
-      fontFamily: 'Arial',
-      color: 'hsl(0, 0%, 30%)',
-    },
-  });
+  cardInfo: {
+    backgroundColor: 'black',
+    borderRadius: 8,
+    shadowOpacity: 0.25,
+    shadowRadius: 5,
+    elevation: 1,
+    margin: 10,
+    padding: 10,
+  },
+  cardImage: {
+    width: '100%',
+    height: 200,
+    borderRadius: 8,
+  },
+  cardTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginTop: 10,
+  },
+  cardText: {
+    fontSize: 14,
+    marginTop: 5,
+  },
+});
 
 export default Card;
